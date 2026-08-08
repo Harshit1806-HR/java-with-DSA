e74cd43dd4a9fb6b5d65c73008fba287bef931c0
Harshit1806-HR <harshittomar376@gmail.com>
2026-08-08 12:03:13 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello {
+       public static void main(String[] var0) {
+              int a = 5;
+
+              System.out.println(a++);
+              System.out.println(++a);
+              System.out.println(a);
+}
+}
