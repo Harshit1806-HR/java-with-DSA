6deadc7084d20ee55ed318c0983cfac43f3e7fe4
Harshit1806-HR <harshittomar376@gmail.com>
2026-08-16 18:01:05 +0530

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,9 +1,26 @@
-class Hello {
-       public static void main(String[] var0) {
-              int a = 5;
+// public class Hello {
 
-              System.out.println(a++);
-              System.out.println(++a);
-              System.out.println(a);
-}
-}
+//        public static void main(String[] args) {
+//               int num[] ={1,8,0,6};
+              
+//               System.out.println(num[1]); 
+//        }
+// }   this is a simple use of array how it works .
+
+
+
+
+// public class Hello{
+//        public static void main(String[] args) {
+              
+//        int num[] = new int[4];
+//                num[0]= 1;
+//                num[1]= 8;    this array program is using for loop 
+//                num[2]= 0;
+//                num[3]= 6;
+
+//               for(int i=0;i<4;i++)
+//        System.out.println(num[i]);
+
+//        }             
+// }
