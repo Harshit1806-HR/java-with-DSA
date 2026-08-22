b26e1b43229a81e2fd12cfbbdc2312e85d0b0093
Harshit1806-HR <harshittomar376@gmail.com>
2026-08-22 13:33:54 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,44 @@
+public class student {
+   int rollno;
+   String name;
+   int marks;
+
+   public static void main(String[] args) {
+      //   student s1 = new student();
+      //   s1.rollno = 18;
+      //   s1.name = "Riya";
+      //    s1.marks = 97; 
+
+      //    student s2 = new student();
+      //   s2.rollno = 03;
+      //   s2.name = "Harshit";
+      //    s2.marks = 92; 
+
+      //    student students[] = new student[2];
+      //    students[0] = s1; 
+      //    students[1] = s2;
+
+         
+      // for(int i = 0; i<students.length; i++)
+      // {
+      //    System.out.println( students[i].rollno + " : " +students[i].name + " : " + students[i].marks);
+      // }
+
+        int nums [] = new int[4];
+        nums[0] = 1;
+        nums[1]= 5; 
+        nums[2]= 8;
+        nums[3]= 7;
+
+        for(int n : nums){      
+         System.out.println(n);  // enhance loop or foreach loop role
+      
+        }
+
+      //   for(int i = 0; i<nums.length;i++)
+      //   {
+      //    System.out.println(nums[i]);
+      //   }
+   }
+}
+   
