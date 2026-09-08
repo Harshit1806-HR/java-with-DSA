cb44ceca2e685ac5527073e823d969d36f8dc1c8
Harshit1806-HR <harshittomar376@gmail.com>
2026-09-08 15:16:50 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,28 @@
+class A{                  //dynamic method dispatch
+    public void show(){
+        System.out.println("in A show");
+    }
+}
+ class B extends A{
+    public void show(){
+        System.out.println("in B show");
+    }   
+ }
+
+ class C extends A{
+    public void show(){
+        System.out.println("in C show");
+    }   
+ }
+public class demo {
+    public static void main (String[]args){
+        A obj = new A();
+        obj.show();
+
+        obj = new B();
+        obj.show();
+
+        obj = new C();
+        obj.show();
+    }                
+}
